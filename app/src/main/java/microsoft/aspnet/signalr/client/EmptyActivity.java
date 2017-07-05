1b7763fded3b4819f0695bb79fba06a5647abc6d
akorzh <androidkorzchalexandr@gmail.com>
2017-07-05 15:23:15 +0300

    - first commit

---
+++ app/src/main/java/microsoft/aspnet/signalr/client/EmptyActivity.java
@@ -0,0 +1,12 @@
+package microsoft.aspnet.signalr.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmptyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
